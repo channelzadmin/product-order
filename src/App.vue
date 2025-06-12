d30962f468e71aaa7a60e28cf125d5512239e64c
channelzadmin <channelz598@gmail.com>
2025-06-12 19:35:18 +0600

    po

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,4 @@
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
